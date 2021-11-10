eef162448686c4448d8971f16baa65a0c2b4a331
Kara <71410027+k-burk@users.noreply.github.com>
2021-11-10 13:24:18 -0700

    Add files via upload

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject Player;
+    public GameObject Camera;
+    private characterScript refScript;
+    private cameraForPickingShitup cameraScript;
+
+    void Start()
+    {
+        refScript = Player.GetComponent<characterScript>();
+        cameraScript = Camera.GetComponent<cameraForPickingShitup>();
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        refScript.enabled = true;
+        cameraScript.enabled = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void Pause()
+    {
+        refScript.enabled = false;
+        cameraScript.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void BackToStart()
+    {
+        refScript.enabled = true;
+        cameraScript.enabled = true;
+        SceneManager.LoadScene("SkyBoxStartScreen");
+    }
+
+    public void LoadMenu()
+    {
+
+    }
+}
---
+++ Assets/SceneSwitcherFuck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcherFuck : MonoBehaviour, orbsYo
+{
+    public void OnStartLook()
+    {
+        Debug.Log("Started looking at");
+    }
+
+    public void OnInteract()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene("Laboratory");
+    }
+
+    public void OnEndLook()
+    {
+        Debug.Log("Stopped Looking at");
+    }
+
+}
